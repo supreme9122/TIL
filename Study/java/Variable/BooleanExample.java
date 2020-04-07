63b421a7686ebc2c3ad08cb29bd60cdd1c1ba0fe
Kim Jae Young <supreme9122@gmail.com>
2020-04-07 20:54:20 +0900

    공부 : Boolean 예제

---
+++ Study/java/Variable/BooleanExample.java
@@ -0,0 +1,14 @@
+
+public class BooleanExample {
+    public static void main(String[] args) {
+        // boolean 타입은 1byte로 표현되는 논리값(true/false)을 저장
+        
+        boolean stop = true;
+        if(stop){
+            System.out.println("중지합니다.");
+        }else{
+            System.out.println("시작합니다.");
+        }
+    }
+
+}
