1a75ae0a7c55dd1037006f9def1590cf737a5aaf
Kim Jae Young <supreme9122@gmail.com>
2020-04-08 21:20:17 +0900

    기능 : 사칙연산 문제번호 10430

---
+++ BaekJoonAlgorithm/Multiplication.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a =  sc.nextInt();
+        int b =  sc.nextInt();
+
+        int c = b%10;
+        int d = b/10%10;
+        int e = b/100;
+        
+        System.out.println(a*c);
+        System.out.println(a*d);
+        System.out.println(a*e);
+        System.out.println(a*b);
+
+        sc.close();
+
+
+    }
+
+}
