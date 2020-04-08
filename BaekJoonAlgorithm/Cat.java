2d81dd0db79f50552cbf14ae2f2b7d14012c93e1
Kim Jae Young <supreme9122@gmail.com>
2020-04-08 21:18:40 +0900

    기능 : 출력 문제번호 10171

---
+++ BaekJoonAlgorithm/Cat.java
@@ -0,0 +1,24 @@
+
+public class Cat {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\"+"\n"+" )  ( ')"+"\n"+"(  /  )"+"\n"+" \\(__)|");
+
+        /* tab                  \t
+        
+        backspace               \b
+        
+        form feed               \f
+        
+        new line                \n
+        
+        carriage return         \r
+        
+        역슬래쉬(\)             \\ 
+        
+        작은따옴표              \' 
+        
+        큰따옴표                \" 
+        
+        유니코드(16진수)문자    \ u유니코드(ex : a = '\u0041') */
+    }
+}
