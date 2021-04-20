69daefa03a56a538c564ef827fe3cafb84d5857c
JaeYoung <supreme9122@gmail.com>
2021-04-20 23:54:43 +0900

    Project : SpringBoot_Member 프로젝트 : 스프링부트_회원객체

---
+++ Projects/springBoot/hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,30 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+		private Long id;
+		private String name;
+		private String pw;
+		
+		
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getPw() {
+			return pw;
+		}
+		public void setPw(String pw) {
+			this.pw = pw;
+		}
+		
+		
+}
