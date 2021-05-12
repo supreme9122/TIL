b0019ae6286d8fadce361020628646b909d8a30a
JaeYoung <supreme9122@gmail.com>
2021-05-12 23:46:46 +0900

    project : springBoot_service 프로젝트 : 스프링부트_서비스 회원가입시 중복 체크 처리 과정

--- Projects/springBoot/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ Projects/springBoot/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -1,5 +1,6 @@
 package hello.hellospring.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import hello.hellospring.domain.Member;
@@ -13,16 +14,46 @@ public class MemberService {
 	
 	
 	/**
-	 * 회원 가입
+	 * 회원 가입		자동주석 단축키 : alt+shift+j
 	 */
 	public Long join(Member member) {
 		//같은 이름이 있는 중복 회원X
-		Optional<Member> result = memberRepository.findByName(member.getName());
-		result.ifPresent(m -> {				//ifPresent는 `Optional로 인해 사용 가능
-			throw new IllegalStateException("이미 존재하는 회원입니다.");
-		});
+//		Optional<Member> result = memberRepository.findByName(member.getName());
+//		result.ifPresent(m -> {				//ifPresent는 Optional로 인해 사용 가능
+//			throw new IllegalStateException("이미 존재하는 회원입니다.");
+//		});
+		
+		
+		validateDuplicateMember(member);	//중복 회원 검증	extract method 단축키 : alt+shift+m
 		
 		memberRepository.save(member);
-		return member.getId();
+			return member.getId();
+		}
+
+
+	private void validateDuplicateMember(Member member) {
+		memberRepository.findByName(member.getName())
+			.ifPresent(m -> {				//위 소스 코드를 지금과 같이 바꿀수 있음.
+				throw new IllegalStateException("이미 존재하는 회원입니다.");
+			});
 	}
+	
+	
+	
+	/**
+	 * 전체 회원 조회
+	 */
+	public List<Member> findMembers() {
+		return memberRepository.findAll(); 
+	}
+	
+	
+	/**
+	 * @param memberId
+	 * 회원이름 찾기
+	 */
+	public Optional<Member> findOne(Long memberId) {
+		return memberRepository.findById(memberId);
+	}
+	
 }
